ed6c3aceb34cd4273195fa133f36f7715de634f3
Julio Francisco Motta <jmottaclaudio@gmail.com>
2020-05-31 17:55:34 -0500

    Primera prueba

---
+++ lcd.c
@@ -0,0 +1,55 @@
+#include <xc.h> 
+
+#define _XTAL_FREQ 20000000UL 
+#define limpiar_lcd 0x01
+#define puertoSalidaLCD LATB
+void comando (void);
+void caracter (void);
+void enable (void);
+void inicioDePuertos(void);
+
+void main (void){
+    inicioDePuertos();
+    int matriz_de_conf[4]= {limpiar_lcd, 0x38, 0x0C, 0x06};
+    int mensaje_muestra[4] = {'P', 'O', 'G', 'L', 'A'};
+    int variableDeCuenta = 0;
+CONFIG: 
+    comando();
+    for(variableDeCuenta = 0 ; variableDeCuenta < 4 ; variableDeCuenta ++){
+       puertoSalidaLCD = matriz_de_conf[variableDeCuenta];
+       enable();
+    }
+    puertoSalidaLCD = 0x85;
+    enable();
+MUESTREO:
+    caracter();
+    for (variableDeCuenta = 0 ; variableDeCuenta < 5 , variableDeCuenta ++){
+        puertoSalidaLCD = mensaje_muestra[variableDeCuenta];
+        enable();
+    }
+    while (1);
+    return;
+}
+
+void inicioDePuertos (void) {
+    TRISB = 0x00;
+    TRISCbits.RC0 = 0; //RS salida 
+    TRISCbits.RC1 = 0; //ENABLE salida 
+}
+
+void comando (void){
+    PORTCbits.RC0 = 0 ; // RS = 0
+    __delay_ms(5);
+}
+
+void caracter (void){
+    PORTCbits.RC0 = 1; //RS = 1
+    __delay_ms(5);
+}
+
+void enable (void){
+    PORTCbits.RC1 = 1; // Enable = 1
+    __delay_ms(3);
+    PORTCbits.RC1 = 0; // Enable = 0 
+    __delay_ms(2); 
+}
