edffff59df174a7c87c2b94a525638df8a561714
Julio Francisco Motta <jmottaclaudio@gmail.com>
2020-06-01 14:17:50 -0500

    Añadimos el siguiente puerto de leds

--- leds.c
+++ leds.c
@@ -2,8 +2,10 @@
 #include <xc.h> 
 
 #define _XTAL_FREQ 20000000
-#define puertoDeLeds PORTB 
+#define puertoDeLeds LATB 
 #define trisDePuertoDeLeds TRISB 
+#define puertoDeLeds2 LATD
+#define trisDePuertoDeLeds2 TRISD 
 
 void inicioPuertos (void);
 int variableDeCuenta = 0;
@@ -13,10 +15,12 @@ void main (void){
 CUENTA:
     for (variableDeCuenta = 0 ; variableDeCuenta < 255 ; variableDeCuenta ++){
         puertoDeLeds = variableDeCuenta;
+        puertoDeLeds2 = ~variableDeCuenta;
         __delay_ms(100);
     }
 }
 
 void inicioPuertos (void){
     trisDePuertoDeLeds = 0x00;
+    trisDePuertoDeLeds2 = 0x00;
 }
