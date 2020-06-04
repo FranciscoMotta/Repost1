e7ce91573a6c9d3a2004b9b7512b040ed7e877da
FranciscoMotta <59737000+FranciscoMotta@users.noreply.github.com>
2020-06-03 22:24:12 -0500

    Código sin sentido para probar el push pull
    XD

--- lcd2.c
+++ lcd2.c
@@ -1,1 +1,15 @@
 Esta parte es solo para probar XD
+void main (void){
+int xd = 0;
+int uwu = 0;
+char lol = 0x00;
+for (xd = 0 ; xd < 10 ; xd ++){
+ uwu = xd + 1;
+ if (uwu == 5){
+    lol = 0xFF
+ } else {
+  lol = 0xAA;
+ }
+}
+
+}
